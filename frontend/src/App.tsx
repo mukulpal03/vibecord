928255e59ed5de2876c045052a789b10a2b0ac4c
Mukul Pal <mukulpal0333@gmail.com>
2025-07-07 13:47:52 +0530

    feat: add shadcn and tailwind for ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1>Vibecord</h1>
+      <Button variant={"default"}>Click me</Button>
     </>
   );
 }
